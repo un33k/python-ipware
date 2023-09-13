333e2a27974affb81bf946be6eb6dba4536ee0f1
Trenton Holmes <797416+stumpylog@users.noreply.github.com>
2023-09-13 11:12:07 -0700

    Combine and standardize to pyproject.toml for all things

--- ipware/__init__.py
+++ ipware/__init__.py
@@ -1,1 +1,2 @@
 from .ipware import IpWare  # noqa
+from .__version__ import __version__  # noqa
--- ipware/__version__.py
+++ ipware/__version__.py
@@ -1,8 +1,1 @@
-__title__ = "python-ipware"
-__author__ = "Val Neekman"
-__author_email__ = "info@neekware.com"
-__description__ = "A Python package to retrieve user's IP address"
-__url__ = "https://github.com/un33k/python-ipware"
-__license__ = "MIT"
-__copyright__ = "Copyright 2023 Val Neekman @ Neekware Inc."
 __version__ = "0.9.0"
